c566ca77feccff5446f48b3fe0dd314517d3098e
Matthew Morgenegg <noxpert@gmail.com>
2024-11-26 19:35:02 -0700

    Finish basic setup based on:
    https://dev.to/chandrapantachhetri/docker-postgres-node-typescript-setup-47db

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,34 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import { Pool } from "pg";
 
 const app = express();
 dotenv.config(); //Reads .env file and makes it accessible via process.env
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.send("hi");
+});
+
+app.get("/dbtest", (req: Request, res: Response, next: NextFunction) => {
+  connectToDB().then(r => res.send("Connected to the database!")).catch(err => {
+    console.log(err)
+    res.send("Failed to connect to the database!");
+  });
 });
 
 app.listen(process.env.PORT, () => {
